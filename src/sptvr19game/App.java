0e7b9e8374876ba1acbd16caa8df1b2b77d2879f
none <none@ee.ee>
2020-09-16 09:57:39 +0300

    aya

--- src/sptvr19game/App.java
+++ src/sptvr19game/App.java
@@ -5,10 +5,40 @@
  */
 package sptvr19game;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author user
  */
 class App {
+
+    private int attempt;
+    public void run(){
+        System.out.println("Угадай задуманное число.");
+        System.out.println("------------------------");
+        int min = 0;
+        int max = 10;
+        Random random = new Random();
+        int myNumber = random.nextInt(5-0+1)+0;
+        System.out.println("Задумай число от 0 до 5. Угадай!");
+        Scanner scanner = new Scanner(System.in);
+        do{
+            int gamerNumber = scanner.nextInt();
+            if (myNumber == gamerNumber) {
+                System.out.println("Ты выйграл!");
+                break;
+            } else {
+                if (attempt >= 3){ 
+                    System.out.println("Не угадал, попробуй еще: ");
+                }else{
+                        
+                        
+                        }
+            }
+        }while()
+        
+    
     
 }
